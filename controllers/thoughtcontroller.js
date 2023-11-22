a0fe8aae7fa80c0318f9fbe5617a9f1959d930cc
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-11-22 18:11:37 -0500

    Updated Thought Controller

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -15,13 +15,13 @@ module.exports = {
     }
   },
   async getThoughtsByID(req, res) {
-    const params = req.params[0];
+    const {thoughtId} = req.params;
     try{
-      const thought = await Thought.findOne({_id: params[0]})
+      const thought = await Thought.findOne({_id: thoughtId})
       .select('-__v')
       .lean();
       
-      if(!thought) res.status(404).json({message: `No thought found with the ID : ${params[0]}`})
+      if(!thought) res.status(404).json({message: `No thought found with the ID : ${thoughtId}`})
       
       const thoughtObj = {
         thought,
@@ -42,11 +42,11 @@ module.exports = {
     }
   },
   async deleteThoughtById(req, res) {
-    const params = req.params[0];
+    const {thoughtId} = req.params;
     try{
-      const thought = await Thought.findOneAndRemove({_id: params[0]});
+      const thought = await Thought.findOneAndRemove({_id: thoughtId});
       
-      if(!thought) res.status(404).json({message: `No thought found with the ID : ${params[0]}`})
+      if(!thought) res.status(404).json({message: `No thought found with the ID : ${thoughtId}`})
       
       return res.json({message: `User ${thought.username} successfully deleted`});
     } catch (err){
@@ -55,15 +55,15 @@ module.exports = {
     }
   },
   async updateThoughtsById(req, res) {
-    const params = req.params[0];
+    const {thoughtId} = req.params;
     try{
       const thought = await Thought.findOneAndUpdate(
-        { _id: params[0]},
+        { _id: thoughtId},
         { $set: req.body },
         {runValidators: true, new: true}
       );
       
-      if(!thought) res.status(404).json({message: `No thought found with the ID : ${params[0]}`})
+      if(!thought) res.status(404).json({message: `No thought found with the ID : ${thoughtId}`})
       
       const thoughtObj = {
         thought,
@@ -75,15 +75,39 @@ module.exports = {
     }
   },
   async addReaction(req, res) {
+    const {thoughtId} = req.params;
     try {
-      res.status(404).json({message: 'Coming Soon'}); //TODO Adding Friend Logic
+      const thought = await Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+      
+      if(!thought) return res.status(404).json({message: `No thought found with the ID : ${thoughtId}`})
+      
+      const thoughtObj = {
+        thought,
+      }
+      return res.status(200).json(thoughtObj);
     } catch (err) {
       res.status(500).json(err);
     }
   },
   async removeReaction(req, res) {
+    const {thoughtId, reactionId} = req.params;
     try {
-      res.status(404).json({message: 'Coming Soon'}); //TODO Adding Friend Logic
+      const thought = await Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        { $pull: { reactions: {reactionId: reactionId} } },
+        { runValidators: true, new: true }
+      );
+      
+      if(!thought) return res.status(404).json({message: `No thought found with the ID : ${thoughtId}`})
+      
+      const thoughtObj = {
+        thought,
+      }
+      return res.status(200).json(thoughtObj);
     } catch (err) {
       res.status(500).json(err);
     }
