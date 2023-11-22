2f5f4b63f24968d9ba569066f4f5e788c61117ae
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-11-21 20:50:56 -0500

    Installed Moment to handle Date Formatting

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,33 @@
-const {Schema, model} = require('mongoose');
+const {Schema, model, Types} = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    require: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function(value) {
+      return moment(value).format('YYYY-MM-DD HH:mm a');
+    }
+  },
+},
+  {
+    toJSON: {
+      getters: true,
+    },
+  });
 
 const thoughtSchema = new Schema({
   reactionBody: {
@@ -14,11 +43,14 @@ const thoughtSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now,
-    //TODO Use a getter method to format the timestamp on query
+    get: function(value) {
+      return moment(value).format('YYYY-MM-DD HH:mm a');
+    }
   },
-  //TODO move Reaction schema here
+  reactions: [reactionSchema]
 })
 
 const Thought = model('thought', thoughtSchema)
+
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,22 +1,28 @@
-const {Schema, model} = require('mongoose');
+const {Schema, model, Types} = require('mongoose');
 const thoughtSchema = require('./Thought')
 
 const userSchema = new Schema({
   username: {
     type: String,
-    //TODO is Unique
+    unique: true,
     require: true,
     max_length: 50,
   },
   email: {
     type: String,
-    //TODO is Unique
+    unique: true,
     require: true,
-    //TODO Email Validation
+    match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
   },
   thoughts: [thoughtSchema],
-  // TODO friends: [userSchema],
-})
+  friends: [
+    {
+      type: Types.ObjectId,
+      ref: 'User',
+    },
+    ],
+  },
+);
 
 const User = model('user', userSchema)
 
